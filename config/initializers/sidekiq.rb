e871a4f7e270ec6190a379f256f5d82191732703
井上貴之 <takayuki@mbp.local>
2019-07-30 14:11:27 +0900

    optimized for docker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379", namespace: "sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379", namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379", namespace: "sidekiq_#{Rails.env}" }
 end
 
 Sidekiq::Logging.logger = Logger.new(Rails.root + "log/sidekiq.log", 3, 10 * 1024 * 1024)
