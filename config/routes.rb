3c099c565901247a924fb97080ef6d7f40954144
TakayukI <takayukiinoue116@gmail.com>
2020-07-29 07:35:56 +0900

    routing restaurant_data_sets#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :search_restaurants, only: [:new]
   resources :restaurant_data_sets, only: %i[index new]
+  get "/restaurant_data_sets/deleted", to: "restaurant_data_sets#destroy"
   get "/restaurant_data_sets/sent_message", to: "restaurant_data_sets#sent_message"
   get "/restaurant_data_sets/:cache_id", to: "restaurant_data_sets#show"
   get "/restaurant_data_sets/:restaurant_data_set_id/complete", to: "restaurant_data_sets#create"
@@ -24,7 +25,7 @@ Rails.application.routes.draw do
     post "/line/send_message"
     post "validate_chat_unit", to: "validate_chat_unit#create"
     resources :custom_restaurants, only: [:create]
-    resources :restaurant_data_sets, only: [:create]
+    resources :restaurant_data_sets, only: %i[create destroy]
     post "/restaurant_data_subsets/create_text"
   end
 end
